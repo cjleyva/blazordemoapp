a252e101fa866a38d2ace5c87792d735da2428d0
cjleyva <cjleyva0505@gmail.com>
2024-02-29 21:39:42 -0500

    primer commit

--- obj/Debug/net8.0/blazordemoapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/blazordemoapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazordemoapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d3ae29fb948f77ce9584c9ca7993a5fed27cf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazordemoapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazordemoapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
